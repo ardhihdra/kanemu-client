38c1496fe70881b4087490b6636d8fadd9d757bb
ardhi.rofi <ardhi.rofi@elnusa.co.id>
2021-09-20 19:58:42 +0700

    change bg, add contact

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
 import logo from '../assets/img/logo/logo-horizontal.png';
 import OrderForm from './OrderForm';
+import ig from '../assets/img/028-instagram.png';
+import wa from '../assets/img/041-whatsapp.png';
 import './Home.css';
 
 export default function Home() {
 
 
     return (
-        <div>
+        <div className="pages-container">
             <img src={logo} className="App-logo" alt="logo" />
-            <div className="txt-header ds-pb-5">Order Now!</div>
+            <div className="txt-header ds-pb-5">Ramah di Harga, Mewah di Rasa!</div>
             <div className="ds-border form-container ds-shadow ds-p-3">
                 <OrderForm/>
+            </div>
+            <div className="txt-subheader contact-info ds-pt-6">
+                <div className="contact-flex">
+                    <img className="logo" alt="instagram" src={ig}></img>
+                    <div className="grow2 ds-ml-3">Follow us : kanemufreshdrink.id</div>
+                    <img className="logo ds-ml-2" alt="whatsapp" src={wa}></img>
+                    <div className="grow2 ds-ml-3">Order. Info & Kemitraan <br/>+62 896 62362510</div>
+                </div>
+                <div className="ds-m-6 hashtag">
+                    #menemuKANEMU #KANEMUan #menyegarKANEMU
+                </div>
             </div>
         </div>
     )
